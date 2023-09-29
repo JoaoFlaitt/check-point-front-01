fbcddf7ed6c89cd825eeaf8551fdbd2f750b869d
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 08:43:13 -0300

    finalizando cp

---
+++ script.js
@@ -0,0 +1,22 @@
+let nome = "Joao"
+let idade = 18
+let solteiro = true
+let valorProduto = 10.5
+
+console.log(nome);
+console.log(idade);
+console.log(solteiro);
+console.log(valorProduto);
+
+let num1 = 10;
+let num2 = 5;
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+
+console.log("O resultado da operação é: " + soma);
+console.log("O resultado da operação é: " + subtracao);
+console.log("O resultado da operação é: " + multiplicacao);
+console.log("O resultado da operação é: " + divisao);
